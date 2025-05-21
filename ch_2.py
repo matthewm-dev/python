e3bd8f750bd2d13bde33286ac06b552c9a656373
matthewm-dev <contact@matthewm.dev>
2025-05-21 15:12:52 -0400

    Testing

---
+++ ch_2.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 18 11:34:03 2025
+
+@author: matthew
+"""
+
+print('Yankees rule!')
+print('But not in Boston!')
+print('Yankees rule,', 'but not in Boston!')
+
+x, y = 2, 3
+x, y = y, x
+print('x =', x)
+print('y =', y)
